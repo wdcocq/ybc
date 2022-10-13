7cf4392a258274a98f4028cb81d3100d1f1d5364
Wester de Cocq <w.decocq@gmail.com>
2022-10-13 23:29:46 +0200

    Change dropdown to func component and add router Link option

--- src/components/dropdown.rs
+++ src/components/dropdown.rs
@@ -1,8 +1,68 @@
+use std::rc::Rc;
+
 use yew::prelude::*;
 
-use crate::elements::button::Button;
-
 #[derive(Clone, Debug, Properties, PartialEq)]
+pub struct ControlledDropdownProps {
+    /// The content of the dropdown menu.
+    ///
+    /// This content will be placed directly within the `div.dropdown-content` container.
+    #[prop_or_default]
+    pub children: Children,
+    #[prop_or_default]
+    pub classes: Option<Classes>,
+    /// Make this dropdown triggerable based on hover.
+    #[prop_or_default]
+    pub hoverable: bool,
+    /// The content of the trigger.
+    #[prop_or_default]
+    pub trigger_html: Html,
+    /// Can be used to active or get the current active state of the dropdown
+    pub active: UseStateHandle<bool>,
+}
+
+/// An interactive dropdown menu for discoverable content. Can be controlled from outside through
+/// the active property.
+///
+/// [https://bulma.io/documentation/components/dropdown/](https://bulma.io/documentation/components/dropdown/)
+#[function_component(ControlledDropdown)]
+pub fn activated_dropdown(ControlledDropdownProps { children, classes, hoverable, trigger_html, active }: &ControlledDropdownProps) -> Html {
+    let classes = classes!(
+        "dropdown",
+        classes.clone(),
+        hoverable.then_some("is-hoverable"),
+        active.then_some("is-active"),
+    );
+
+    let opencb = Callback::from({
+        let active = active.clone();
+        move |_| active.set(true)
+    });
+
+    let closecb = Callback::from({
+        let active = active.clone();
+        move |_| active.set(false)
+    });
+
+    html! {
+        <div class={classes}>
+            if **active {
+                <div onclick={closecb}
+                     style="z-index:10;background-color:rgba(0,0,0,0);position:fixed;top:0;bottom:0;left:0;right:0;"/>
+            }
+            <div class="dropdown-trigger" onclick={opencb}>
+                {trigger_html.clone()}
+            </div>
+            <div class="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                    {children.clone()}
+                </div>
+            </div>
+        </div>
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Properties)]
 pub struct DropdownProps {
     /// The content of the dropdown menu.
     ///
@@ -14,77 +74,115 @@ pub struct DropdownProps {
     /// Make this dropdown triggerable based on hover.
     #[prop_or_default]
     pub hoverable: bool,
-    /// Any additional classes to use for the trigger button.
+    /// The content of the trigger.
     #[prop_or_default]
-    pub button_classes: Option<Classes>,
-    /// The content of the trigger button.
-    #[prop_or_default]
-    pub button_html: Html,
-}
-
-/// Dropdown actions.
-pub enum DropdownMsg {
-    Open,
-    Close,
+    pub trigger_html: Html,
 }
 
 /// An interactive dropdown menu for discoverable content.
 ///
 /// [https://bulma.io/documentation/components/dropdown/](https://bulma.io/documentation/components/dropdown/)
-pub struct Dropdown {
-    is_menu_active: bool,
+#[function_component(Dropdown)]
+pub fn dropdown(DropdownProps { children, classes, hoverable, trigger_html }: &DropdownProps) -> Html {
+    let active = use_state_eq(|| false);
+
+    html! {
+        <ControlledDropdown
+            classes={classes.clone()}
+            hoverable={*hoverable}
+            trigger_html={trigger_html.clone()}
+            {active}>
+            {children.clone()}
+        </ControlledDropdown>
+    }
 }
 
-impl Component for Dropdown {
-    type Message = DropdownMsg;
-    type Properties = DropdownProps;
+#[derive(Debug, Clone, PartialEq)]
+pub enum DropdownItemTags {
+    A(Rc<str>),
+    Div,
+}
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { is_menu_active: false }
-    }
+#[derive(Debug, Clone, PartialEq, Properties)]
+pub struct DropdownItemProps {
+    #[prop_or_default]
+    pub children: Children,
+    #[prop_or_default]
+    pub classes: Classes,
+    #[prop_or_else(|| DropdownItemTags::Div)]
+    pub tag: DropdownItemTags,
+}
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        if ctx.props().hoverable {
-            return false;
-        }
-        match msg {
-            DropdownMsg::Open => self.is_menu_active = true,
-            DropdownMsg::Close => self.is_menu_active = false,
-        }
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let props = ctx.props();
-        let link = ctx.link();
-        let mut classes = Classes::from("dropdown");
-        classes.push(&props.classes);
-        let opencb = if props.hoverable {
-            classes.push("is-hoverable");
-            Callback::noop()
-        } else {
-            link.callback(|_| DropdownMsg::Open)
-        };
-        let overlay = if self.is_menu_active {
-            classes.push("is-active");
-            html! {<div onclick={link.callback(|_| DropdownMsg::Close)} style="z-index:10;background-color:rgba(0,0,0,0);position:fixed;top:0;bottom:0;left:0;right:0;"></div>}
-        } else {
-            html! {}
-        };
-        html! {
+#[function_component(DropdownItem)]
+pub fn dropdown_item(DropdownItemProps { children, classes, tag }: &DropdownItemProps) -> Html {
+    let classes = classes!("dropdown-item", classes.clone());
+    match tag {
+        DropdownItemTags::A(href) => html! {
+            <a class={classes} href={href.clone()}>
+                {children.clone()}
+            </a>
+        },
+        DropdownItemTags::Div => html! {
             <div class={classes}>
-                {overlay}
-                <div class="dropdown-trigger">
-                    <Button classes={props.button_classes.clone()} onclick={opencb}>
-                        {props.button_html.clone()}
-                    </Button>
-                </div>
-                <div class="dropdown-menu" role="menu">
-                    <div class="dropdown-content">
-                        {props.children.clone()}
-                    </div>
-                </div>
+                {children.clone()}
             </div>
+        },
+    }
+}
+
+#[function_component(DropdownDivider)]
+pub fn dropdown_divider() -> Html {
+    html! {
+        <hr class="dropdown-divider"/>
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+#[cfg(feature = "router")]
+mod router {
+    use super::*;
+    use serde::Serialize;
+    use yew_router::prelude::*;
+
+    #[derive(Debug, Clone, PartialEq, Properties)]
+    pub struct DropdownLinkProps<R, Q = ()>
+    where
+        R: Routable,
+        Q: Clone + PartialEq + Serialize,
+    {
+        #[prop_or_default]
+        pub children: Children,
+        #[prop_or_default]
+        pub classes: Classes,
+        /// Route that will be pushed when the anchor is clicked.
+        pub to: R,
+        /// Route query data
+        pub query: Option<Q>,
+        pub disabled: bool,
+        /// [`NodeRef`] for the <a> element.
+        pub anchor_ref: NodeRef,
+    }
+
+    #[function_component(DropdownLink)]
+    pub fn dropdown_link<R, Q = ()>(DropdownLinkProps { children, classes, to, query, disabled, anchor_ref }: &DropdownLinkProps<R, Q>) -> Html
+    where
+        R: Routable + 'static,
+        Q: Clone + PartialEq + Serialize + 'static,
+    {
+        let classes = classes!("dropdown-item", classes.clone());
+
+        html! {
+            <Link<R, Q> {classes}
+                to={to.clone()}
+                query={query.clone()}
+                disabled={*disabled}
+                {anchor_ref}>
+                {children.clone()}
+            </Link<R, Q>>
         }
     }
 }
+
+#[cfg(feature = "router")]
+pub use router::DropdownLink;
